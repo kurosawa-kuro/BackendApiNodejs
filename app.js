e72d7dabebdd5afba653dc14cfea37db34678cbc
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-12-24 17:12:34 +0900

    API

---
+++ app.js
@@ -0,0 +1,47 @@
+// NPM
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+// Security
+const { SESSION_SECRET } = require("./config/app.config.js").security;
+
+// Log
+const accesslogger = require("./lib/log/accesslogger.js");
+const systemlogger = require("./lib/log/systemlogger.js");
+
+// Error Handling
+const clientError = require("./lib/errorHandling/clientError");
+const serverError = require("./lib/errorHandling/serverError");
+
+const app = express();
+
+app.disable("x-powered-by");
+
+app.use(accesslogger());
+
+app.use(cookieParser());
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  name: "sid"
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routing
+app.use("/", (() => {
+  const router = express.Router();
+  router.use("/", require("./routes/index"));
+  router.use("/users", require("./routes/users"));
+  return router;
+})());
+
+app.use(systemlogger());
+
+app.use(clientError());
+app.use(serverError());
+
+app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  const param = {"result":"Hello"};      // レスポンスで返す値。JSON形式。
+  res.header('Content-Type', 'application/json; charset=utf-8')  // 「レスポンスはJSON形式で返すよ」の意味
+  res.send(param);       
+});
+
+module.exports = router;
