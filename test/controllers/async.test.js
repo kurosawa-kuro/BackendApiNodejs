35afab27a9a2b4b9ee146311fd7bd7cf7aad202d
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-01-10 05:30:46 +0900

    ダブルからシングルに統一

--- test/controllers/async.test.js
+++ test/controllers/async.test.js
@@ -1,21 +1,21 @@
-const request = require("supertest");
-const app = require("../../app");
+const request = require('supertest');
+const app = require('../../app');
 
-const commonModule = require("../../lib/commonModule");
+const commonModule = require('../../lib/commonModule');
 
-describe("非同期テスト", () => {
+describe('非同期テスト', () => {
   beforeEach(() => {
     jest.setTimeout(30000);
   });
 
-  test("It should response the GET method", async () => {
-    const response = await request(app).get("/async");
+  test('It should response the GET method', async () => {
+    const response = await request(app).get('/async');
 
-    console.log("before await commonModule.sleep(6000)");
+    console.log('before await commonModule.sleep(6000)');
 
     await commonModule.sleep(6000);
 
-    console.log("after await commonModule.sleep(6000)");
+    console.log('after await commonModule.sleep(6000)');
 
     expect(response.statusCode).toBe(200);
   });
--- test/controllers/sync.test.js
+++ test/controllers/sync.test.js
@@ -1,10 +1,10 @@
-const request = require("supertest");
-const app = require("../../app");
+const request = require('supertest');
+const app = require('../../app');
 
-describe("同期テスト", () => {
-  test("It should response the GET method", done => {
+describe('同期テスト', () => {
+  test('It should response the GET method', done => {
     request(app)
-      .get("/sync")
+      .get('/sync')
       .then(response => {
         expect(response.statusCode).toBe(200);
         done();
