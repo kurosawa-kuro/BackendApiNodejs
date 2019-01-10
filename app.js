95298dc9b78565a8d3c67b479034242a3f3c73c1
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-01-10 13:04:31 +0900

    routesに移動できるものを整理

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const { PORT } = require('./config/constants');
 // var numCPUs = require('os').cpus().length;
 
 // Library
-const commonModule = require('./lib/commonModule');
-const requestBody = require('./lib/middleware/requestBody');
+const { displayConstants } = require('./lib/commonModule');
 
 // Log
 const accessLogger = require('./lib/log/accessLogger.js');
@@ -17,13 +16,13 @@ const systemLogger = require('./lib/log/systemLogger.js');
 
 const app = express();
 
+// Middleware
 require('./config/middleware')(app);
 
 // Access Log
 app.use(accessLogger());
 
 // Routing
-app.use(requestBody());
 require('./config/routes.js')(app);
 
 // System Log
@@ -32,16 +31,13 @@ app.use(systemLogger());
 // Error Handling
 require('./config/errorHandling')(app);
 
-// express-list-endpoints
-commonModule.displayListEndpoints(app);
-
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, 'localhost', async err => {
     if (err) {
       console.log('errr starting server', err);
     }
     console.log('App listening on port %s', PORT);
-    await commonModule.displayConstants();
+    await displayConstants();
   });
 }
 
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,17 @@
+// Middleware
+const requestBody = require('../lib/middleware/requestBody');
+
+// Library
+const commonModule = require('../lib/commonModule');
+
 module.exports = function (app) {
+  // Display Request Body
+  app.use(requestBody());
+
+  // Routes
   app.use('/sync', require('../app/controllers/sync_controller'));
   app.use('/async', require('../app/controllers/async_controller'));
+
+  // express-list-endpoints
+  commonModule.displayListEndpoints(app);
 };
