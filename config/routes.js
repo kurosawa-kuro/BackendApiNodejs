028270cd5114ec5fba07da44bf37ae092fbf2ad9
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-01-10 17:58:54 +0900

    コントローラの追加

--- config/routes.js
+++ config/routes.js
@@ -11,8 +11,10 @@ module.exports = function (app) {
   // Routes
   app.use('/sync', require('../app/controllers/sync_controller'));
   app.use('/async', require('../app/controllers/async_controller'));
-  app.use('/crud', require('../app/controllers/crud_controller'));
-
+  app.use('/crud_bookshelf', require('../app/controllers/crud_bookshelf_controller'));
+  // app.use('/crud_sequelize', require('../app/controllers/crud_sequelize_controller'));
+  // app.use('/crud_bookshelf_mongo', require('../app/controllers/crud_bookshelf_mongo_controller'));
+  // app.use('/crud_sequelize_mongo', require('../app/controllers/crud_sequelize_mongo_controller'));
 
   // express-list-endpoints
   commonModule.displayListEndpoints(app);
